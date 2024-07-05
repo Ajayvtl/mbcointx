6057c15dc0ab3782917227329d287091a544a3c4
Ajayvtl <145452656+Ajayvtl@users.noreply.github.com>
2024-07-05 23:45:24 +0530

    withmobile

---
+++ dist/dist/dist/dist/dist/dist/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const transactionRoutes = require('./routes/transactionRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'views')));
+app.use('/transaction', transactionRoutes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
